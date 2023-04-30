766c4c5e49e08b8e610674d329f1f43fc99bb261
Jaison E Mathew <jaisonemathew14@gmail.com>
2023-04-30 21:59:38 +0530

    added files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from './components/header.jsx'
+import Meme from './components/meme.jsx'
+import './App.css'
+
+function App() {
+ 
+  return (
+    <div>
+      <Header />
+      <Meme />
+    </div>
+  )
+}
+
+export default App
